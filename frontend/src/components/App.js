0556a0f82cfe60b5108e96ff5b9c0f97f6534331
Henry <spike.1@live.co.uk>
2018-02-02 14:25:49 +0000

    Add Post component and App optimisations
    - Make app stateless
    - Include two placeholder posts

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,41 +1,79 @@
 import React, { Component } from 'react';
 import './App.css';
+//components
+import Post from './Post';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="container">
-				<div class="row">
-					<div class="col-sm-12">
-						<h1>Readable</h1>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-sm-4">
-						<h2>
-							React
-							<small>View categorie individually</small>
-						</h2>
-						{/* React categorie */}
-					</div>
-					<div class="col-sm-4">
-						<h2>
-							Redux
-							<small>View categorie individually</small>
-						</h2>
-						{/* Redux categorie */}
-					</div>
-					<div class="col-sm-4">
-						<h2>
-							Udacity
-							<small>View categorie individually</small>
-						</h2>
-						{/* Udacity categorie */}
-					</div>
+const App = (props) => {
+	const post1 = {
+    id: '8xf0y6ziyjabvozdd253nd',
+    timestamp: 1467166872634,
+    title: 'Udacity is the best place to learn React',
+    body: 'Everyone says so after all.',
+    author: 'thingtwo',
+    category: 'react',
+    voteScore: 6,
+    deleted: false,
+    commentCount: 2
+  }
+	const post2 = {
+    id: '6ni6ok3ym7mf1p33lnez',
+    timestamp: 1468479767190,
+    title: 'Learn Redux in 10 minutes!',
+    body: 'Just kidding. It takes more than 10 minutes to learn technology.',
+    author: 'thingone',
+    category: 'redux',
+    voteScore: -5,
+    deleted: false,
+    commentCount: 0
+  }
+  return (
+    <div className="container">
+			<div class="row">
+				<div class="col-sm-12">
+					<h1>Readable</h1>
 				</div>
 			</div>
-    );
-  }
+			<div class="row">
+				<div class="col-sm-12 col-md-6 col-lg-4">
+					<h2>
+						React
+						<small>View categorie individually</small>
+					</h2>
+					{/* React categorie */}
+					<Post
+						timestamp={post1.timestamp}
+						title={post1.title}
+						body={post1.body}
+						author={post1.author}
+						voteScore={post1.voteScore}
+						commentCount={post1.commentCount}
+					/>
+				</div>
+				<div class="col-sm-12 col-md-6 col-lg-4">
+					<h2>
+						Redux
+						<small>View categorie individually</small>
+					</h2>
+					{/* Redux categorie */}
+					<Post
+						timestamp={post2.timestamp}
+						title={post2.title}
+						body={post2.body}
+						author={post2.author}
+						voteScore={post2.voteScore}
+						commentCount={post2.commentCount}
+					/>
+				</div>
+				<div class="col-sm-12 col-md-6 col-lg-4">
+					<h2>
+						Udacity
+						<small>View categorie individually</small>
+					</h2>
+					{/* Udacity categorie */}
+				</div>
+			</div>
+		</div>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/Post.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const Post = (props) => {
+	const { timestamp, title, body, author, voteScore, commentCount } = props;
+  return (
+		<div className="card fluid">
+			<div className="section left">
+				<h3>
+					{title}
+					<small>- {author}</small>
+				</h3>
+				<p>{body}</p>
+			</div>
+			<div className="container section">
+				<div class="row">
+					<div class="col-sm-4">{voteScore}</div>
+					<div class="col-sm-4">
+						{
+							commentCount == 1
+							? `${commentCount} comment`
+							: `${commentCount} comments`
+						}
+					</div>
+					<div class="col-sm-4">{timestamp}</div>
+				</div>
+			</div>
+		</div>
+  );
+}
+
+export default Post;
