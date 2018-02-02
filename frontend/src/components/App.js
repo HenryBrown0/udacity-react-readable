afcf34b629725f4834b76bd45ffb5b4d3bfa0ae9
Henry <spike.1@live.co.uk>
2018-02-02 20:07:34 +0000

    Split Category Panel to new component

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -3,10 +3,10 @@ import { connect } from 'react-redux'
 import { fetchPosts } from '../actions';
 import './App.css';
 //components
-import Post from './Post';
+import CategoryPanel from './CategoryPanel';
 
 const App = (props) => {
-	const { react, redux, udacity, fetchPosts } = props;
+	const { categories, fetchPosts } = props;
 	return (
 		<div className="container">
 			<div className="row">
@@ -14,73 +14,17 @@ const App = (props) => {
 					<h1 onClick={() => fetchPosts()}>Readable</h1>
 				</div>
 			</div>
+
 			<div className="row">
-				<div className="col-sm-12 col-md-6 col-lg-4">
-					<h2>
-						React
-						<small>View categorie individually</small>
-					</h2>
-					{/* React categorie */}
-					{
-						react ?
-							react.map(p =>
-								<Post
-									key={p.id}
-									timestamp={p.timestamp}
-									title={p.title}
-									body={p.body}
-									author={p.author}
-									voteScore={p.voteScore}
-									commentCount={p.commentCount}
-								/>
-							)
-						: <p>No posts</p>
-					}
-				</div>
-				<div className="col-sm-12 col-md-6 col-lg-4">
-					<h2>
-						Redux
-						<small>View categorie individually</small>
-					</h2>
-					{/* Redux categorie */}
-					{
-						redux ?
-							redux.map(p =>
-								<Post
-									key={p.id}
-									timestamp={p.timestamp}
-									title={p.title}
-									body={p.body}
-									author={p.author}
-									voteScore={p.voteScore}
-									commentCount={p.commentCount}
-								/>
-							)
-						: <p>No posts</p>
-					}
-				</div>
-				<div className="col-sm-12 col-md-6 col-lg-4">
-					<h2>
-						Udacity
-						<small>View categorie individually</small>
-					</h2>
-					{/* Udacity categorie */}
-					{
-						udacity ?
-							udacity.map(p =>
-								<Post
-									key={p.id}
-									timestamp={p.timestamp}
-									title={p.title}
-									body={p.body}
-									author={p.author}
-									voteScore={p.voteScore}
-									commentCount={p.commentCount}
-								/>
-							)
-						: <p>No posts</p>
-					}
-				</div>
+			{
+				categories.map(c =>
+					<CategoryPanel
+						key={c.category}
+						category={c.category}
+						posts={c.posts}
+					/>
+				)
+			}
 			</div>
 		</div>
 	);
@@ -88,9 +32,18 @@ const App = (props) => {
 
 function mapStateToProps ({ posts }) {
   return {
-		react: posts.react,
-		redux: posts.redux,
-		udacity: posts.udacity
+		categories: [
+			{
+				category: 'React',
+				posts: posts.react
+			},{
+				category: 'Redux',
+				posts: posts.redux
+			},{
+				category: 'Udacity',
+				posts: posts.udacity
+			}
+		]
   }
 }
 
---
+++ frontend/src/components/CategoryPanel.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './App.css';
+//components
+import Post from './Post';
+
+const CategoryPanel = (props) => {
+	const { category, posts } = props;
+	return (
+		<div className="col-sm-12 col-md-6 col-lg-4">
+			<h2>
+				{category}
+				<small>View categorie individually</small>
+			</h2>
+			{
+				posts ? posts.length !== 0 ?
+					posts.map(p =>
+						<Post
+							key={p.id}
+							timestamp={p.timestamp}
+							title={p.title}
+							body={p.body}
+							author={p.author}
+							voteScore={p.voteScore}
+							commentCount={p.commentCount}
+						/>
+					)
+				: <p>No posts</p> : <p>No posts</p>
+			}
+		</div>
+	);
+}
+
+export default CategoryPanel;
