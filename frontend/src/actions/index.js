ca182fa43d57bc2fbf5d55cf18c361b38a5021ef
Henry <spike.1@live.co.uk>
2018-02-02 18:37:35 +0000

    Add thunk to project

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -1,9 +1,26 @@
+import axios from 'axios';
 export const ADD_POST = 'ADD_POST'
+export const FETCH_POSTS = 'FETCH_POSTS'
+
+const URL = 'http://localhost:3001/';
+const headers = { 'Authorization': 'whatever-you-want' };
 
 export function addPost ({ id, title }) {
 	return {
 		type: ADD_POST,
 		id,
 		title,
+	}
+}
+
+export function fetchPosts(){
+	const request = axios.get(`${URL}posts`,{headers});
+	return dispatch => {
+		request.then(({data})=> {
+			dispatch({
+				type: FETCH_POSTS,
+				payload: data
+			})
+		})
 	}
 }
