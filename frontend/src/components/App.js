a8f457d3f7d36d82655c8ad800270ac709132fe7
Henry <spike.1@live.co.uk>
2018-02-02 13:43:55 +0000

    Created default view skeleton

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -4,8 +4,35 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-				<h1>Readable</h1>
+      <div className="container">
+				<div class="row">
+					<div class="col-sm-12">
+						<h1>Readable</h1>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-sm-4">
+						<h2>
+							React
+							<small>View categorie individually</small>
+						</h2>
+						{/* React categorie */}
+					</div>
+					<div class="col-sm-4">
+						<h2>
+							Redux
+							<small>View categorie individually</small>
+						</h2>
+						{/* Redux categorie */}
+					</div>
+					<div class="col-sm-4">
+						<h2>
+							Udacity
+							<small>View categorie individually</small>
+						</h2>
+						{/* Udacity categorie */}
+					</div>
+				</div>
 			</div>
     );
   }
