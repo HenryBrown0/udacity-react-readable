77d3c0b0fc02efa55e1021f05830ecde370f38c1
Henry <spike.1@live.co.uk>
2018-02-04 14:12:13 +0000

    Change size of post page

--- frontend/src/components/Pages/Post.js
+++ frontend/src/components/Pages/Post.js
@@ -8,8 +8,8 @@ const Category = ({ match }) => {
 	const category = match.params.category;
 	const postID = match.params.postID;
   return(
-		<div className="col-sm-12 col-md-6 col-lg-4 col-md-offset-3
-			col-lg-offset-4">
+		<div className="col-sm-12 col-md-8 col-lg-6 col-md-offset-2
+			col-lg-offset-3">
 			<GetPost category={category} postID ={postID}/>
 		</div>
 	)
