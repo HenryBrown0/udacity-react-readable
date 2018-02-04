5ba23e76f0511c2651079d6dccea8fd18b2ee795
Henry <spike.1@live.co.uk>
2018-02-04 12:01:18 +0000

    Styled categorie links

--- frontend/src/components/Pages/Categories.js
+++ frontend/src/components/Pages/Categories.js
@@ -15,18 +15,16 @@ class Categories extends Component {
 	render() {
 		const { categories } = this.props;
 		return (
-			<div>
-				<div className="row">
-					<ul>
-					{
-						categories
-							? categories.map(c =>
-								<li key={c.path}>
-									<Link to={"/category/"+c.path+"/"}>{c.name}</Link>
-								</li>)
-							: null
-					}
-					</ul>
+			<div className="row center">
+				<div className="col-sm-12 col-md-6 col-lg-4 col-md-offset-3
+					col-lg-offset-4">
+				{
+					categories ? categories.map(c =>
+						<Link key={c.path} to={"/category/"+c.path+"/"} className="button">
+							{c.name}
+						</Link>
+					) : null
+				}
 				</div>
 			</div>
 		);
