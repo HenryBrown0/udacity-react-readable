8df9051ed6f646f77937c4acbf69145d3672b4f4
Henry <spike.1@live.co.uk>
2018-02-12 13:38:24 +0000

    Add logs to console on comment actions

--- frontend/src/actions/comments.js
+++ frontend/src/actions/comments.js
@@ -11,6 +11,7 @@ export function fetchPostComments(postID){
 	const request = axios.get(`${URL}posts/${postID}/comments`,{headers});
 	return dispatch => {
 		request.then(({data}) => {
+			console.log("Fetch comments successful")
 			dispatch({
 				type: FETCH_POST_COMMENTS,
 				postComments: data,
@@ -18,7 +19,7 @@ export function fetchPostComments(postID){
 			})
 		})
 		.catch(function (error) {
-    	console.log(error);
+    	console.error(error);
   	});
 	}
 }
@@ -27,13 +28,14 @@ export function addComment(newComment){
 	const request = axios.post(`${URL}comments`,newComment,{headers});
 	return dispatch => {
 		request.then((response) => {
+			console.log("Add comment successful")
 			dispatch({
 				type: ADD_COMMENT,
 				newComment
 			})
 		})
 		.catch(function (error) {
-    	console.log(error);
+    	console.error(error);
   	});
 	}
 }
@@ -42,47 +44,49 @@ export function deleteComment(deleteComment){
 	const request = axios.delete(`${URL}comments/${deleteComment.id}`,{headers});
 	return dispatch => {
 		request.then((response) => {
+			console.log("Delete comment successful")
 			dispatch({
 				type: DELETE_COMMENT,
 				deleteComment
 			})
 		})
 		.catch(function (error) {
-    	console.log(error);
+    	console.error(error);
   	});
 	}
 }
 
-export function voteComment(vote){ 
+export function voteComment(vote){
 	const request = axios
 		.post(`${URL}comments/${vote.comment.id}`,{ option: vote.type },{headers});
 	return dispatch => {
 		request.then((response) => {
+			console.log("Vote comment successful")
 			dispatch({
 				type: UPDATE_COMMENT,
 				comment: response.data
 			})
 		})
 		.catch(function (error) {
-    	console.log(error);
+    	console.error(error);
   	});
 	}
 }
 
 export function editComment(edit){
-	console.log(edit)
 	const request = axios
 		.put(`${URL}comments/${edit.id}`,
 			{ timestamp: edit.timestamp, body: edit.body  },{headers});
 	return dispatch => {
 		request.then((response) => {
+			console.log("Edit comment successful")
 			dispatch({
 				type: UPDATE_COMMENT,
 				comment: response.data
 			})
 		})
 		.catch(function (error) {
-    	console.log(error);
+    	console.error(error);
   	});
 	}
 }
