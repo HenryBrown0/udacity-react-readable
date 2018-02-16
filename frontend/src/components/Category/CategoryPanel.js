4ce0fa40cd8732b744ccdba14fd5bf6af89cbe35
Henry <spike.1@live.co.uk>
2018-02-16 16:39:06 +0000

    Add order posts

--- frontend/src/components/Category/CategoryPanel.js
+++ frontend/src/components/Category/CategoryPanel.js
@@ -1,5 +1,5 @@
 //React
-import React from 'react';
+import React, { Component } from 'react';
 //Redux
 import { connect } from 'react-redux';
 //components
@@ -7,22 +7,70 @@ import '../App.css';
 import BasicPost from '../Post/Basic';
 
 //Content
-const CategoryPanel = (props) => {
-	const { category, posts } = props;
-	const categoryPosts = posts[category] ? posts[category].length !== 0
-		? posts[category].filter(p => p.category === category)
-		: [] : []
-	return (
-		<div>
-		{
-			categoryPosts.length !== 0 ?
-				categoryPosts.map(p =>
-					<BasicPost key={p.id} post={p} />
-				)
-			: <p>No posts</p>
-		}
-		</div>
-	);
+class CategoryPanel extends Component {
+	state = {
+    order: 'dateNewest',
+  };
+
+	handleChange = event => {
+    this.setState({ order: event.target.value });
+  };
+
+	dateNewest = (a, b) => {
+		return b.timestamp  - a.timestamp;
+	};
+
+	dateOldest = (a, b) => {
+		return a.timestamp  - b.timestamp;
+	};
+
+	voteHighest = (a, b) => {
+		return b.voteScore  - a.voteScore;
+	};
+
+	voteLowest = (a, b) => {
+		return a.voteScore  - b.voteScore;
+	};
+
+	render(){
+
+		const { category, posts } = this.props;
+		const categoryPosts = posts[category] ? posts[category].length !== 0
+			? posts[category].filter(p => p.category === category)
+			: [] : []
+
+		const { order } = this.state;
+
+		return (
+			<div>
+				<form>
+					<fieldset>
+						<legend>Order By</legend>
+						<div className="input-group vertical">
+							<select
+								id="orderBy"
+								value={order}
+								name="order"
+								onChange={this.handleChange}
+							>
+								<option value="dateNewest">Date [newest]</option>
+								<option value="dateOldest">Date [oldest]</option>
+								<option value="voteHighest">Vote [hightest]</option>
+								<option value="voteLowest">Vote [lowest]</option>
+							</select>
+						</div>
+					</fieldset>
+				</form>
+				{
+					categoryPosts.length !== 0 ?
+						categoryPosts.sort(this[order]).map(p =>
+							<BasicPost key={p.id} post={p} />
+						)
+					: <p>No posts</p>
+				}
+			</div>
+		);
+	}
 }
 
 function mapStateToProps ({ posts }) {
